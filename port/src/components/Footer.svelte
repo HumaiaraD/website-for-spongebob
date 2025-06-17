772aa15fda2d862c52877de07034110dcea0a8e2
Humaiara Diea <d.humaiara@gmail.com>
2025-06-16 22:21:57 -0400

    Update Footer.svelte

--- port/src/components/Footer.svelte
+++ port/src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="py-20 sm:py-32 bg-sky-700 border-t border-solid border-violet-950
                flex flex-col gap-4 sm:gap-8 justify-center items-center">
         <p class="px-4 py-2 bg-yellow-300 text-slate-950 font-medium">
-            Connect with me &darr;
+            This project is only for practice, not monetizing. Connect with me &darr;
         </p>
         <div class="flex flex-col gap-4 items-center justify-center">
             <p><b class="pr-2">Email:</b>spongebob@squarepants.com</p>
